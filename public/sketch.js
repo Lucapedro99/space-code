316362e8d5dde149f598ded979d83d038e266401
Lucapedro99 <luca1.pedroni@mail.polimi.it>
2022-11-23 13:44:15 +0100

    commit

--- public/sketch.js
+++ public/sketch.js
@@ -1,10 +1,42 @@
+let clientSocket = io ();
+
+clientSocket.on("connect", newConnection)
+
+function newConnection() {
+  console.log(clientSocket.id)
+}
+
+
+clientSocket.on("mouseBroadcast", otherMouse)
+
+function otherMouse(dataReceived){
+ console.log(dataReceived)
+  rect(dataReceived.x,dataReceived.y,50,50)
+  fill("red")
+}
+
+
+
+
+function mouseMoved(){
+  let message = {
+    x: mouseX,
+    y: mouseY,
+    id: clientSocket.id,
+  }
+
+  clientSocket.emit("mouseInfo",message);
+}
+
+
 function setup() {
   createCanvas(windowWidth, windowHeight);
 }
 
 function draw() {
-  background(220);
+  //background(20);
+  fill("green")
   rect(mouseX,mouseY,30,30)
-  fill(0,255,0)
+  
 }
 
--- server.js
+++ server.js
@@ -1,5 +1,7 @@
 console.log("script loaded");
 
+const {Socket} = require("engine.io");
+
 let express = require("express");
 
 let app = express();
@@ -8,6 +10,24 @@ let port = 3000;
 
 let server = app.listen(port);
 
-app.use(express.static("public/app sketch"));
+app.use(express.static("public"));
 
-console.log('running server on http://localhost:'+port)
+console.log('running server on http://localhost:'+port);
+
+let serverSocket = require('socket.io');
+
+let io = serverSocket(server);
+
+io.on("connection", newConnection);
+
+function newConnection(newSocket) {
+    console.log(newSocket.id);
+
+    newSocket.on("mouseInfo", mouseReceived);
+
+    function mouseReceived(dataReceived) {
+        console.log(dataReceived);
+        newSocket.broadcast.emit("mouseBroadcast", dataReceived);
+    }
+}
+
